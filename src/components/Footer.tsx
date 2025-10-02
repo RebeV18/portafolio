853f295fc6261c770e2f7383588293f872b06bff
rebev18 <anvargas.dwfs17@bootcampudd.cl>
2025-10-02 01:39:30 -0300

    footer centered

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex flex-col md:flex-row justify-center items-center">
           <div className="mb-4 md:mb-0">
-            <p className="text-sm text-gray-200">
+            <p className="text-xs text-gray-200 text-center">
               © 2025 Portafolio bajo la marca LandingRV.cl de Rebeca
               Vargas. Todos los derechos reservados.
             </p>
